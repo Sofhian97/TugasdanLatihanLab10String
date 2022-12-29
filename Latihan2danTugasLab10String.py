1ef7af387c58f7b23ff4938fe583477c5d4ea9a0
[Sofhian97] <[sofhianp@gmail.com]>
2022-12-29 23:38:47 +0700

    Latihan 2

---
+++ Latihan2danTugasLab10String.py
@@ -0,0 +1,9 @@
+#Cara pertama
+umur = 24
+txt = f"Hello, nama saya john, dan umur saya adalah {umur} tahun"
+print(txt.format(umur))
+
+#Cara kedua
+umur = 24
+txt = "Hello, nama saya john, dan umur saya adalah %d tahun"% 24
+print(txt.format(umur))
