93e7061d9d1818dda018049c963976be93af434f
[Sofhian97] <[sofhianp@gmail.com]>
2022-12-29 23:39:33 +0700

    Latihan 1

---
+++ LatihandanTugasLab10String.py
@@ -0,0 +1,22 @@
+txt = 'Hello World'
+
+# Hitung Jumlah Karakter
+print(len(txt))
+
+#Ambil karakter terakhir
+print(txt[-1])
+
+#Ambil karakter index ke-2 sampai index ke-4 (llo)
+print(txt[2:5])
+
+#Hilangkan spasi pada text berikut
+print(txt.replace(" ",""))
+
+#Ubah Text menjadi huruf besar
+print(txt.upper())
+
+#Ubah text menjadi huruf kecil
+print(txt.lower())
+
+#Ganti karakter H dengan J
+print(txt.replace("H","J"))
